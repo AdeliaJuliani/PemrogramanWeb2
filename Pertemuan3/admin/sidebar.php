8b599cbcffe3f1a571f69843aa331aec4c65742f
AdeliaJuliani <adeliajuliani8@gmail.com>
2025-03-11 20:41:24 +0700

    Menambahkan perubahan

---
+++ Pertemuan3/admin/sidebar.php
@@ -0,0 +1,9 @@
+<div class="sidebar-heading border-bottom bg-light">Start Bootstrap</div>
+                <div class="list-group list-group-flush">
+                    <a class="list-group-item list-group-item-action list-group-item-light p-3" href="panggil.php">Form Nilai</a>
+                    <a class="list-group-item list-group-item-action list-group-item-light p-3" href="panggil2.php">Jadwal MK</a>
+                    <a class="list-group-item list-group-item-action list-group-item-light p-3" href="panggil3.php">Data Mahasiswa</a>
+                    <a class="list-group-item list-group-item-action list-group-item-light p-3" href="panggil4.php">Kalender Akademik</a>
+                    <a class="list-group-item list-group-item-action list-group-item-light p-3" href="panggil5.php">Profile</a>
+                    <a class="list-group-item list-group-item-action list-group-item-light p-3" href="panggil6.php">Akademis</a>
+                </div>
